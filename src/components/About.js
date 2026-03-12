94c72f34aa73f955c8947874f439c319e642d368
Shivam Singh <shivam.y.singh01@gmail.com>
2026-03-12 11:44:54 +0530

    Clean project structure and remove old assets

---
+++ src/components/About.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "../styles/About.css";
+import FadeInSection from "./FadeInSection";
+
+class About extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    const one = (
+      <p>
+        I am currently a <b>Software Engineer</b> at
+        <a href="https://www.jio.com/"> Jio</a>, where I help build
+        autobidder models that make Jio 5g money.
+      </p>
+    );
+    const two = (
+      <p>
+        Outside of work, I’m nerdy about tech gadgets, love literary fiction,
+        and play way too many battle royale games. Oh, I make content too.
+      </p>
+    );
+
+    const tech_stack = [
+      "Java",
+      "MySQL",
+      "React.js",
+      "Python",
+      "Javascript ES6+",
+      "C#",
+    ];
+
+    return (
+      <div id="about">
+        <FadeInSection>
+          <div className="section-header ">
+            <span className="section-title">/ about me</span>
+          </div>
+          <div className="about-content">
+            <div className="about-description">
+              {[one]}
+              {"Here are some technologies I have been working with:"}
+              <ul className="tech-stack">
+                {tech_stack.map(function (tech_item, i) {
+                  return (
+                    <FadeInSection delay={`${i + 1}00ms`}>
+                      <li>{tech_item}</li>
+                    </FadeInSection>
+                  );
+                })}
+              </ul>
+              {[two]}
+            </div>
+         <div className="about-image">
+  <img
+    alt="Shiv"
+    src={process.env.PUBLIC_URL + "/assets/Shiv.jpg"}
+    style={{ width: "300px", height: "auto", display: "block" }}
+  />
+</div>
+          </div>
+        </FadeInSection>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/components/Intro.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import "../styles/Intro.css";
+import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
+import FadeInSection from "./FadeInSection";
+import ParticlePortrait from "./ParticlePortrait";
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: true,
+      activeKey: "1",
+      visible: true,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+  handleSelect(eventKey) {
+    this.setState({
+      activeKey: eventKey,
+    });
+  }
+  render() {
+    return (
+      <div id="intro">
+        <div className="intro-simulation">
+          <ParticlePortrait />
+        </div>
+        <div className="intro-block">
+          <Typist avgTypingDelay={120}>
+            <span className="intro-title">
+              {"hi, "}
+              <span className="intro-name">{"Shiv"}</span>
+              {" here."}
+            </span>
+          </Typist>
+          <FadeInSection>
+            <div className="intro-desc">
+              I'm a software engineer and artist based in Mumbai City. I'm
+              fascinated by large-scale, high-impact products and contributed to
+              major feature launches in industry-leading services as well as
+              apps that have 100M+ users.
+            </div>
+            <a href="mailto:shivam.y.singh01@gmail.com" className="intro-contact">
+              <EmailRoundedIcon></EmailRoundedIcon>
+              {" Say hi!"}
+            </a>
+          </FadeInSection>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Intro;
